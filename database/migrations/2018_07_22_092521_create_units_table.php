54de45dde55907d095bbcdf0e3ec34ffb20a8c41
tomaszbuczekctiplus <tomasz.buczek@ctiplus.pl>
2019-01-14 20:17:47 +0100

    Add Unit: model & migration

---
+++ database/migrations/2018_07_22_092521_create_units_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateUnitsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('units', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('name', 100)->comment('full name unit')->unique();
+            $table->string('short', 10)->comment('short name unit')->unique();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('units');
+    }
+}
