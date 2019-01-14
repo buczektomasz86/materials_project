0ab6dacd35ba045f3f497b4b373de19d6929248f
tomaszbuczekctiplus <tomasz.buczek@ctiplus.pl>
2019-01-14 20:33:50 +0100

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -11,6 +11,22 @@
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', 'IndexController@index');
+
+Route::get('/jednostki_miary', 'UnitController@index');
+Route::get('/jednostki_miary/edytuj/{unit_id}', 'UnitController@edit');
+Route::post('/jednostki_miary/edytuj/{unit_id}', 'UnitController@update');
+Route::get('/jednostki_miary/dodaj', 'UnitController@create');
+Route::post('/jednostki_miary/dodaj', 'UnitController@store');
+
+Route::get('/grupy_materialow', 'GroupController@index');
+Route::get('/grupy_materialow/dodaj', 'GroupController@create');
+Route::post('/grupy_materialow/dodaj', 'GroupController@store');
+Route::get('/grupy_materialow/edytuj/{group_id}', 'GroupController@edit');
+Route::post('/grupy_materialow/edytuj/{group_id}', 'GroupController@update');
+
+Route::get('/materialy', 'MaterialController@index');
+Route::get('/materialy/edytuj/{material_id}', 'MaterialController@edit');
+Route::post('/materialy/edytuj/{material_id}', 'MaterialController@update');
+Route::get('/materialy/dodaj', 'MaterialController@create');
+Route::post('/materialy/dodaj', 'MaterialController@store');
